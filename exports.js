ae7cbeaea60bcc99ff1da766b15bab7db62c4b23
Christian Bundy <me@christianbundy.com>
2014-06-06 16:23:08 -0700

    Reduce complexity of exports.js

--- exports.js
+++ exports.js
@@ -1,33 +1,22 @@
-// Pass the global object by reference
-(function (global) {
-  // Initialize an empty module object to be referenced
-  module = {};
+// Initialize an empty module object
+module = {};
 
-  // Set getters and setters for `module.exports` and the `exports` alias
-  var name = 'exports';
-  if (typeof Object.getOwnPropertyDescriptor(module, name) === 'undefined') {
-    // Extend the global namespace from the local object
-    Object.defineProperty(module, name, {
-      // Set a setter to handle the object's properties
-      set: function (props) {
-        // Export each local variable to the `this` object.
-        for (var prop in props) {
-          // Make sure we aren't inheriting any prototype properties
-          if (props.hasOwnProperty(prop)) {
-            global[prop] = props[prop];
-          }
-        }
-      },
-      get: function () {
-        return global;
+// Functions created this way don't inherit strict mode
+exports = Function('return this')();
+
+// Define what happens when you try to get or set `module.exports`
+Object.defineProperty(module, 'exports', {
+  // Extend the top-level object with the object that's passed
+  set: function (obj) {
+    for (var prop in obj) {
+      // Don't set properties inherited from the prototype
+      if (obj.hasOwnProperty(prop)) {
+        exports[prop] = obj[prop];
       }
-    });
+    }
+  },
+  // Return the top-level object
+  get: function () {
+    return exports;
   }
-  // Set `exports` to the original value of `module.exports`
-  exports = module.exports;
-})((function () {
-  // Pass top-level (global) object to the above function
-  return (function() {
-    return this;
-  }).call(null); // Force `this` to be the top-level object.
-})());
+});
